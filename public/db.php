86d1d873311f4ee1bfc127072885e311e8938833
micael.itaira <tecmidiainfor@gmail.com>
2018-05-07 00:16:12 -0300

    alterando host

--- public/db.php
+++ public/db.php
@@ -1,33 +1,20 @@
 <?php
+    class db{
+        // Variables de conexión
+       private $host = 'mysql762.umbler.com';
+       private $usuario = 'root';
+        private $password = '';
+        private $base = 'test';
+        
 
-	class db{
-		private $host = 'mysql762.umbler.com';
-		private $user = 'micael';
-		private $pass =	'22setembro';
-		private $db   =	'assistec';
+        // Conectar a BD
+        public function conectar(){
+            $conexion_mysql = "mysql:host=$this->host;dbname=$this->base";
+            $conexionDB = new PDO($conexion_mysql, $this->usuario, $this->password);
+            $conexionDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-		public function conectar(){
-			try {
-			
-				$conexao_mysql = "mysql:host=$this->host;dbname=$this->db";
-				$conexaoDB = new PDO($conexao_mysql, $this->user, $this->pass);
-
-			} catch (PDOException $e) {
-				
-				echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
-			
-			}
-
-			
-				
-		}
-
-		//private function __construct(){
-		//	$this->con = new mysqli("localhost","micael"."22setembro","assistec");
-
-		//}
-		//public function query($sql){
-		//	return $this->con->query($sql);
-		//}
-	}
+            //Esta linea arregla la codificacion sino no aparecen en la salida en JSON quedan NULL
+            $conexionDB -> exec("set names utf8");
+            return $conexionDB;
+        }
+    }
